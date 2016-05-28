34d7f035d741095717ca1e40da708c7925367158
leon <leon_a_d@yahoo.com>
2016-05-29 00:58:00 +0100

    simplified extract and sanitise methods

--- lib/sitehub/http_headers.rb
+++ lib/sitehub/http_headers.rb
@@ -1,5 +1,8 @@
 require 'sitehub/constants'
+
 class SiteHub
+  # module HttpHeaders - provides methods for translating HTTP headers names to and
+  # from RACK to HTTP compliant names.
   module HttpHeaders
     include Constants::HttpHeaderKeys
     include Constants
@@ -20,54 +23,83 @@ class SiteHub
                   TRANSFER_ENCODING,
                   CONTENT_ENCODING].freeze
 
-    def split_field(f)
-      f ? f.split(COMMAND_FOLLOWED_BY_SPACES).collect(&:downcase) : []
-    end
-
     def sanitise_headers(src)
-      connections = split_field(src[CONNECTION_HEADER])
+      sanitised_headers = remove_excluded_http_headers(with_http_header_names(src))
 
-      {}.tap do |sanitised_headers|
-        src.each do |key, value|
-          key = key.downcase.gsub(UNDERSCORE, HYPHEN)
-          next if HOP_BY_HOP.member?(key) || connections.member?(key) || SHOULD_NOT_TRANSFER.member?(key)
-          sanitised_headers[key] = value
-        end
-
-        sanitised_headers[LOCATION_HEADER].gsub!(HTTP_OR_SSL_PORT, EMPTY_STRING) if sanitised_headers[LOCATION_HEADER]
-      end
+      modify_location(sanitised_headers) if location_header?(sanitised_headers)
+      sanitised_headers
     end
 
     def extract_http_headers(env)
-      headers = remove_excluded_headers(env)
+      headers = without_excluded_headers(env)
 
-      headers = headers.to_a.each_with_object(Rack::Utils::HeaderHash.new) do |k_v, hash|
-        k, v = k_v
-        hash[reconstruct_header_name(k)] = v
-        hash
+      headers = headers.each_with_object(Rack::Utils::HeaderHash.new) do |key_value, hash|
+        key, value = key_value
+        hash[header_name(key)] = value
       end
 
-      remote_address = env[RackHttpHeaderKeys::REMOTE_ADDRESS_ENV_KEY]
-      headers.merge!(X_FORWARDED_FOR_HEADER => x_forwarded_for_value(headers, remote_address))
+      headers.merge!(X_FORWARDED_FOR_HEADER => x_forwarded_for_value(headers, remote_address(env)))
     end
 
-    def x_forwarded_for_value(headers, remote_address)
-      (forwarded_address_list(headers) << remote_address).join(COMMA_WITH_SPACE)
+    private
+
+    # sanitise headers
+    def remove_excluded_http_headers hash
+      connections = split_field(hash[CONNECTION_HEADER])
+      hash.reject do |key, _value|
+        excluded_header?(key) || connections.member?(key)
+      end
     end
 
+    def http_header_name(key)
+      key.downcase.gsub(UNDERSCORE, HYPHEN)
+    end
+
+    def modify_location headers
+      headers[LOCATION_HEADER].gsub!(HTTP_OR_SSL_PORT, EMPTY_STRING)
+    end
+
+    def location_header?(sanitised_headers)
+      sanitised_headers[LOCATION_HEADER]
+    end
+
+    def with_http_header_names hash
+      hash.each_with_object({}) do |array, _hash|
+        key, value = array
+        _hash[http_header_name(key)] = value
+      end
+    end
+
+    def excluded_header?(key)
+      HOP_BY_HOP.member?(key) || SHOULD_NOT_TRANSFER.member?(key)
+    end
+
+    def split_field(field)
+      field ? field.split(COMMAND_FOLLOWED_BY_SPACES).collect(&:downcase) : []
+    end
+
+    # for extract_http_headers
     def forwarded_address_list(headers)
       headers[X_FORWARDED_FOR_HEADER].to_s.split(COMMAND_FOLLOWED_BY_SPACES)
     end
 
-    def remove_excluded_headers(env)
-      env.reject do |k, v|
-        !RackHttpHeaderKeys::HTTP_HEADER_FILTER_EXCEPTIONS.include?(k.to_s.upcase) &&
-          (!RACK_HTTP_HEADER_ID.match(k) || v.nil?)
+    def header_name(name)
+      name.sub(HTTP_PREFIX, EMPTY_STRING).gsub(UNDERSCORE, HYPHEN)
+    end
+
+    def remote_address(env)
+      env[RackHttpHeaderKeys::REMOTE_ADDRESS_ENV_KEY]
+    end
+
+    def without_excluded_headers(env)
+      env.reject do |key, value|
+        !RackHttpHeaderKeys::HTTP_HEADER_FILTER_EXCEPTIONS.include?(key.to_s.upcase) &&
+            (!RACK_HTTP_HEADER_ID.match(key) || value.nil?)
       end
     end
 
-    def reconstruct_header_name(name)
-      name.sub(HTTP_PREFIX, EMPTY_STRING).gsub(UNDERSCORE, HYPHEN)
+    def x_forwarded_for_value(headers, remote_address)
+      (forwarded_address_list(headers) << remote_address).join(COMMA_WITH_SPACE)
     end
   end
 end
