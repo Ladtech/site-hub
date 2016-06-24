62bef8d6604702718729bf2f44f12bfe04140fa1
leon <leon_a_d@yahoo.com>
2016-06-24 11:51:04 +0100

    tidy up

--- lib/sitehub/forward_proxy.rb
+++ lib/sitehub/forward_proxy.rb
@@ -1,9 +1,11 @@
 # rubocop:disable Metrics/ParameterLists
 class SiteHub
   class ForwardProxy
-    include Rules, Resolver
+    include Rules, Resolver, Equality
 
-    attr_reader :downstream_client, :sitehub_cookie_name, :sitehub_cookie_path, :id, :mapped_path, :mapped_url
+    attr_reader :downstream_client, :sitehub_cookie_name, :id, :sitehub_cookie_path, :mapped_path, :mapped_url
+
+    transient :downstream_client
 
     def initialize(sitehub_cookie_path: nil, sitehub_cookie_name:, id:, rule: nil, mapped_path:, mapped_url:)
       @downstream_client = DownstreamClient.new
@@ -19,25 +21,19 @@ class SiteHub
       request = env[REQUEST]
       request.map(mapped_path, mapped_url)
 
-      response = downstream_client.call(request)
-      value = request.cookies[sitehub_cookie_name.to_s] || id
-      response.set_cookie(sitehub_cookie_name, path: resolve_sitehub_cookie_path(request), value: value)
-      response
+      downstream_client.call(request).tap do |response|
+        response.set_cookie(sitehub_cookie_name,
+                            path: resolve_sitehub_cookie_path(request),
+                            value: resolve_sitehub_cookie_value(request))
+      end
+    end
+
+    def resolve_sitehub_cookie_value(request)
+      request.cookies[sitehub_cookie_name.to_s] || id
     end
 
     def resolve_sitehub_cookie_path(request)
       sitehub_cookie_path || request.path
-    end
-
-    def ==(other)
-      expected = [other.mapped_path,
-                  other.mapped_url,
-                  other.rule,
-                  other.id,
-                  other.sitehub_cookie_name,
-                  other.sitehub_cookie_path]
-
-      expected == [mapped_path, mapped_url, rule, id, sitehub_cookie_name, sitehub_cookie_path]
     end
   end
 end
